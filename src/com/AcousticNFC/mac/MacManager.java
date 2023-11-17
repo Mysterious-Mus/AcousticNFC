99e2813d785be0291736221e061877d66bbe9061
Junzhe Yu <yujzh1@shanghaitech.edu.cn>
2023-11-17 15:59:51 +0800

    new backoff logic: let the slower plug in

--- src/com/AcousticNFC/mac/MacManager.java
+++ src/com/AcousticNFC/mac/MacManager.java
@@ -289,7 +289,8 @@ public class MacManager {
                         Random random = new Random();
                         Thread.sleep(Configs.BACKOFF_UNIT.v() * 
                             random.nextInt((int)Math.pow(2, 
-                                Math.min(backoffTimes, Configs.BACKOFF_MAX_TIMES.v()))));
+                                // Math.min(backoffTimes, Configs.BACKOFF_MAX_TIMES.v()))));
+                                Math.min(0, Configs.BACKOFF_MAX_TIMES.v() + 1 - backoffTimes))));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
